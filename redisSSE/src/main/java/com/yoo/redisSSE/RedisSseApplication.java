cd9b28a72f2e0a669c35332c80aa2213b264745e
yoo <edel1212@naver.com>
2024-06-01 16:32:23 +0900

    project init

---
+++ redisSSE/src/main/java/com/yoo/redisSSE/RedisSseApplication.java
@@ -0,0 +1,13 @@
+package com.yoo.redisSSE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisSseApplication.class, args);
+	}
+
+}
---
+++ redisSSE/src/test/java/com/yoo/redisSSE/RedisSseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoo.redisSSE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
