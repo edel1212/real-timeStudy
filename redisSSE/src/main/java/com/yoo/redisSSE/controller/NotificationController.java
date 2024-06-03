c73a3f7ff6ef4ec928f5413de6c4157c9495a8c8
jeoungho.yoo <jeoungho.yoo@stans.co.kr>
2024-06-03 17:55:04 +0900

    fix code

--- redisSSE/src/main/java/com/yoo/redisSSE/controller/NotificationController.java
+++ redisSSE/src/main/java/com/yoo/redisSSE/controller/NotificationController.java
@@ -1,8 +1,11 @@
 package com.yoo.redisSSE.controller;
 
 import com.yoo.redisSSE.service.NotificationService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -12,8 +15,16 @@ public class NotificationController {
 
     private final NotificationService notificationService;
 
-    @GetMapping(path = "/api/v1/notifications/stream")
-    public SseEmitter streamNotifications(String username) {
-        return notificationService.createEmitter(username);
+    @GetMapping(value = "/sub", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe(HttpServletRequest request, String accountId){
+        // ℹ️ 해더 값에 Token을 넣어서 왔을 경우도 수정이 가능하다.
+        String authorization =  request.getHeader("Authorization");
+        if(authorization != null){ } // TODO Jwt 로직 수행
+        return notificationService.subscribe(accountId);
+    }
+
+    @PostMapping(value = "/send-data", produces = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public void sendData(String accountId, String message) {
+        notificationService.notify(accountId, message);
     }
 }
--- redisSSE/src/main/java/com/yoo/redisSSE/service/NotificationService.java
+++ redisSSE/src/main/java/com/yoo/redisSSE/service/NotificationService.java
@@ -1,42 +1,25 @@
 package com.yoo.redisSSE.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+public interface NotificationService {
+    // 타임 아웃 시간
+    Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
 
-@Service
-@RequiredArgsConstructor
-public class NotificationService {
-    private final Map<String, SseEmitter> userEmitters = new ConcurrentHashMap<>();
+    /**
+     * <h3>클라이언트가 구독을 위해 호출하는 메서드.</h3>
+     *
+     * @param accountId - 구독하는 클라이언트의 사용자 아이디.
+     * @return SseEmitter - 서버에서 보낸 이벤트 Emitter
+     */
+    SseEmitter subscribe(String accountId);
 
-    public SseEmitter createEmitter(String username) {
-        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
-        userEmitters.put(username, emitter);
-
-        emitter.onCompletion(() -> userEmitters.remove(username));
-        emitter.onTimeout(() -> userEmitters.remove(username));
-        emitter.onError((e) -> userEmitters.remove(username));
-
-        return emitter;
-    }
-
-    public void sendNotification(String username, String message) {
-        sendRealTimeNotification(username, message);
-    }
-
-    private void sendRealTimeNotification(String username, String message) {
-        SseEmitter emitter = userEmitters.get(username);
-        if (emitter != null) {
-            try {
-                emitter.send(SseEmitter.event().name("notification").data(message));
-            } catch (Exception e) {
-                // 예외 처리
-            }
-        }
-    }
-
-
+    /**
+     * 서버의 이벤트를 클라이언트에게 보내는 메서드
+     * 다른 서비스 로직에서 이 메서드를 사용해 데이터를 Object event에 넣고 전송하면 된다.
+     *
+     * @param accountId - 메세지를 전송할 사용자의 아이디.
+     * @param event     - 전송할 이벤트 객체.
+     */
+    void notify(String accountId, Object event);
 }
