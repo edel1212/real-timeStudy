2d46f4bce5d8ada2598a1efa00211f1cbd4d48b5
jeoungho.yoo <jeoungho.yoo@stans.co.kr>
2024-06-04 18:01:17 +0900

    미사용 코드 제거

--- redisSSE/src/main/java/com/yoo/redisSSE/config/RedisConfig.java
+++ redisSSE/src/main/java/com/yoo/redisSSE/config/RedisConfig.java
@@ -1,9 +1,6 @@
 package com.yoo.redisSSE.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
