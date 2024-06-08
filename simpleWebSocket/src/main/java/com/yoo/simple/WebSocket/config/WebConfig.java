60284b3288a390e4a16bf8fc3e6c443462af8988
yoo <edel1212@naver.com>
2024-06-08 22:27:39 +0900

    Cors 설정 추가

---
+++ simpleWebSocket/src/main/java/com/yoo/simple/WebSocket/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.yoo.simple.WebSocket.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    /**
+     * Cors 설정
+     * - Spring Security를 사용할 경우 Security내에서 설정해주자!
+     *  ㄴ> Spring Security가 활성화된 경우, 기본적으로 HTTP 요청에 대한 보안을 제어하고 CORS 정책도 처리하기 떄문임
+     * */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .maxAge(3600);
+    }
+}
