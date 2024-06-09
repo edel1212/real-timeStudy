a8af766de0a016938fef2edf586fb5f51a4af4ef
yoo <edel1212@naver.com>
2024-06-09 17:01:30 +0900

    Controller Repository 연결

--- simpleWebSocket/src/main/java/com/yoo/simple/WebSocket/controller/ChatRoomController.java
+++ simpleWebSocket/src/main/java/com/yoo/simple/WebSocket/controller/ChatRoomController.java
@@ -1,6 +1,7 @@
 package com.yoo.simple.WebSocket.controller;
 
 import com.yoo.simple.WebSocket.dto.ChatRoom;
+import com.yoo.simple.WebSocket.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,26 +16,26 @@ import java.util.List;
 @RequestMapping("/chat")
 public class ChatRoomController {
 
+    private final ChatRoomRepository chatRoomRepository;
+
     // 모든 채팅방 목록 반환
     @GetMapping("/rooms")
     public ResponseEntity<List<ChatRoom>> room() {
-        //List<ChatRoom> list = chatRoomRepository.findAllRoom();
-        return ResponseEntity.ok(null);
+        List<ChatRoom> list = chatRoomRepository.findAllRoom();
+        return ResponseEntity.ok(list);
     }
 
     // 채팅방 생성
     @PostMapping("/room")
-    public ChatRoom createRoom( String user1, String user2) {
-        //return chatRoomRepository.createChatRoom(user1, user2);
-        return  null;
+    public ChatRoom createRoom(String roomName) {
+        return chatRoomRepository.createChatRoom(roomName);
     }
 
 
     // 특정 채팅방 조회
     @GetMapping("/room/{roomId}")
     public ChatRoom roomInfo(@PathVariable String roomId) {
-        //return chatRoomRepository.findRoomById(roomId);
-        return  null;
+        return chatRoomRepository.findRoomById(roomId);
     }
 
 }
