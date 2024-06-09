2201c9e039cd8dccaf8e2856a0e6583380b44ce2
yoo <edel1212@naver.com>
2024-06-09 17:00:48 +0900

    Repository 구현

---
+++ simpleWebSocket/src/main/java/com/yoo/simple/WebSocket/repository/ChatRoomRepository.java
@@ -0,0 +1,42 @@
+package com.yoo.simple.WebSocket.repository;
+
+import com.yoo.simple.WebSocket.dto.ChatRoom;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Log4j2
+@Repository
+@RequiredArgsConstructor
+public class ChatRoomRepository {
+    private final Map<String, ChatRoom> chatRoomMap = new ConcurrentHashMap();
+
+    public List<ChatRoom> findAllRoom() {
+        List chatRooms = new ArrayList(chatRoomMap.values());
+        // 최신 순으로 정렬
+        Collections.reverse(chatRooms);
+        return chatRooms;
+    }
+
+    public ChatRoom findRoomById(String id) {
+        return chatRoomMap.get(id);
+    }
+
+    public ChatRoom createChatRoom(String name) {
+        String roomId = UUID.randomUUID().toString();
+        log.info("-----------");
+        log.info("roomId ::: {}",roomId);
+        log.info("-----------");
+        ChatRoom chatRoom = ChatRoom.builder()
+                .roomId(roomId)
+                .name(name)
+                .build();
+        chatRoomMap.put(roomId, chatRoom);
+        return chatRoom;
+    }
+
+
+}
