67a9ada0cd442ad81178a04eb3064ff26543969f
jeoungho.yoo <jeoungho.yoo@stans.co.kr>
2024-06-10 16:36:13 +0900

    가독성을 위한 코드 위치 변경

--- simpleWebSocket/src/main/java/com/yoo/simple/WebSocket/config/WebSockConfig.java
+++ simpleWebSocket/src/main/java/com/yoo/simple/WebSocket/config/WebSockConfig.java
@@ -10,18 +10,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 //  웹소켓 활성화
 @EnableWebSocketMessageBroker
 public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
-    /**
-     * WebSocket 메시징을 설정할 때 호출됩니다. 주로 메시지 브로커의 설정을 담당
-     * */
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        //  메모리 기반 메시지 브로커를 활성화
-        // /sub로 시작하는 목적지로 클라이언트가 구독한 경우 이 브로커가 메시지를 처리합니다.
-        registry.enableSimpleBroker("/sub");
-        // 애플리케이션에서 메시지를 보내기 위한 목적지 경로의 접두사를 설정합니다.
-        // 서버는 이 접두사를 보고 해당 메시지를 애플리케이션의 메시지 처리기로 라우팅합니다.
-        registry.setApplicationDestinationPrefixes("/pub");
-    }
 
     /**
      * WebSocket STOMP 엔드포인트를 등록하기 위해 사용됩니다.
@@ -39,4 +27,18 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
                 .withSockJS()
         ;
     }
+
+    /**
+     * WebSocket 메시징을 설정할 때 호출됩니다. 주로 메시지 브로커의 설정을 담당
+     * */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //  메모리 기반 메시지 브로커를 활성화
+        // /sub로 시작하는 목적지로 클라이언트가 구독한 경우 이 브로커가 메시지를 처리합니다.
+        registry.enableSimpleBroker("/sub");
+        // 애플리케이션에서 메시지를 보내기 위한 목적지 경로의 접두사를 설정합니다.
+        // 서버는 이 접두사를 보고 해당 메시지를 애플리케이션의 메시지 처리기로 라우팅합니다.
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+
 }
