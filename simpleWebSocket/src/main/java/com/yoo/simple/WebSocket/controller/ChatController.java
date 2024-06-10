75db8b685a0d0bbb0a0fe92203b05cbf26a54b4d
jeoungho.yoo <jeoungho.yoo@stans.co.kr>
2024-06-10 17:15:27 +0900

    @SendTo 추가

--- simpleWebSocket/src/main/java/com/yoo/simple/WebSocket/controller/ChatController.java
+++ simpleWebSocket/src/main/java/com/yoo/simple/WebSocket/controller/ChatController.java
@@ -6,6 +6,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
@@ -22,6 +23,7 @@ public class ChatController {
      * - 입장, 글쓰기 모두 이곳을 통해 전달 된다.
      * */
     @MessageMapping("/chat/message")
+    //@SendTo("구독 주소") // ℹ️ 해당 메서드를 타면 지정 구독자들에게 같은 메세지가 전달 가능하다.
     public void message(ChatMessage message,
                         @Header("Authorization") String authHeader,
                         @Headers Map<String, Object> headers) {
