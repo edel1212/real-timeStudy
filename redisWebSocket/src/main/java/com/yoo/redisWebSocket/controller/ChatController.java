604278c1f9e63089efc61726ee4ec86f52436a69
jeoungho.yoo <jeoungho.yoo@stans.co.kr>
2024-06-11 16:35:16 +0900

    메세지 전송 코드 수정

--- redisWebSocket/src/main/java/com/yoo/redisWebSocket/controller/ChatController.java
+++ redisWebSocket/src/main/java/com/yoo/redisWebSocket/controller/ChatController.java
@@ -5,35 +5,26 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
-
-import java.util.Map;
 
 @Log4j2
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
-    private final SimpMessageSendingOperations messagingTemplate;
     private final RedisTemplate<String, Object> redisTemplate;
 
     @Value("${redis.ssePrefix}")
     private String channelPrefix;
 
     @MessageMapping("/chat/message")
-    //@SendTo("구독 주소") // ℹ️ 해당 메서드를 타면 지정 구독자들에게 같은 메세지가 전달 가능하다.
-    public void message(ChatMessage message,
-                        @Header("Authorization") String authHeader,
-                        @Headers Map<String, Object> headers) {
-
-        // Pub
-        redisTemplate.convertAndSend(getChannelName(message.getRoomId()), message);
+    public void message(ChatMessage message) {
+        // 1. RoomId를 가져옴
+        String roomId = message.getRoomId();
+        // 2 . roomId -> Redis 식별키 변경
+        String topic = channelPrefix + roomId;
+        // 3 . 구독자(topic)들에게 emmit
+        redisTemplate.convertAndSend(topic , message);
     }
 
-    private String getChannelName(String id) {
-        return channelPrefix + id;
-    }
 }
