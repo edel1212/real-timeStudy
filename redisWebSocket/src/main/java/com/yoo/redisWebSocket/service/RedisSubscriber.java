ad3e7efb5c0169133b176c3e1de20f1a1dd76b52
jeoungho.yoo <jeoungho.yoo@stans.co.kr>
2024-06-14 15:22:54 +0900

    주석 추가

--- redisWebSocket/src/main/java/com/yoo/redisWebSocket/service/RedisSubscriber.java
+++ redisWebSocket/src/main/java/com/yoo/redisWebSocket/service/RedisSubscriber.java
@@ -16,34 +16,33 @@ import java.io.IOException;
 @RequiredArgsConstructor
 @Component
 public class RedisSubscriber implements MessageListener {
-
     private final ObjectMapper objectMapper;
-    // STOMP 주입
+    // ℹ️ STOMP 주입 (메세지를 전송하기 위한 의존성 주입)
     private final SimpMessageSendingOperations messagingTemplate;
     @Value("${redis.ssePrefix}")
     private String channelPrefix;
 
     /**
-     * 구독자들에게 <pre>convertAndSend() 시</pre> 사용 될 메서드
+     * 구독자들에게 <pre>convertAndSend() 시</pre> 사용 될 메서드이다
      * */
     @Override
     public void onMessage(Message message, byte[] pattern) {
         try {
             // 1 . Redis에서만 사용했던 prefix 제거
-            String channel = new String(message.getChannel())
+            String roomId = new String(message.getChannel())
                     .substring(channelPrefix.length());
 
-            log.info("channel ::: {}  ",channel);
+            log.info("channel ::: {}  ",roomId);
             
             // 2 . RedisValue -> JavaObject 변환
             ChatMessage chatMessage = objectMapper.readValue(message.getBody(),
                     ChatMessage.class);
 
             // 3 . 구독자들에게 Stomp를 사용해서 메세지 전송
-            messagingTemplate.convertAndSend("/sub/chat/room/" + channel, chatMessage);
+            messagingTemplate.convertAndSend("/sub/chat/room/" + roomId, chatMessage);
 
         } catch (IOException e) {
             log.error("IOException is occurred. ", e);
-        }
+        } // try - catch
     }
 }
